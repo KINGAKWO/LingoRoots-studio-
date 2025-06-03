43d9de85a0efe22075aa79c9780f2d156b307bbe
AKWO MAKEMBE KING <kingakwomakembe@gmail.com>
2025-06-03 14:42:28 +0000

    the app has been loading for a longtime now and shows nothing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import { AuthRedirect } from "@/components/auth-redirect";
 
 export default function HomePage() {
   return (
-    <AuthRedirect redirectTo="/dashboard" requireAuth={false} authPath="/login">
-      {/* This content will ideally not be shown as redirect should happen */}
+    <AuthRedirect redirectTo="/dashboard" requireAuth={true} authPath="/login">
+      {/* This content will ideally not be shown as AuthRedirect's internal loader will take precedence during redirection. */}
       <div className="flex items-center justify-center min-h-screen">
         <p>Loading LingoRoots...</p>
       </div>
